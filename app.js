865f8bbc98e6719423bdade69a98734ff28b7b11
hrishiballal <hrishikeshballal@yahoo.com>
2023-06-05 17:54:17 +0100

    Update app.js

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json())
 
-// var baseurl = 'http://local.test:8000/api/v1/projects/';
-var baseurl = 'https://www.geodesignhub.com/api/v1/projects/';
+var baseurl = 'http://local.test:8000/api/v1/projects/';
+// var baseurl = 'https://www.geodesignhub.com/api/v1/projects/';
 
 function getSystemName(systemsResponse, diagramSystem) {
     let systemName = '';
